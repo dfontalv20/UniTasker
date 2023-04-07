b224fbb222c28462edea8d9df32add9a7887e1d1
Diego Fontalvo <d.andres2002@hotmail.com>
2023-04-07 18:36:04 -0500

    feat: Added navigation bar

--- app/src/main/java/com/example/unitasker/MainActivity.kt
+++ app/src/main/java/com/example/unitasker/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.unitasker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var frameLayout: FrameLayout? = null
+    private var navbar: BottomNavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main)
+        frameLayout = findViewById(R.id.main_layout)
+        navbar = findViewById(R.id.navbar)
+        navbar?.setOnItemSelectedListener { item ->
+            handleNavbarItemSelection(item)
+        }
+    }
+
+    private fun handleNavbarItemSelection(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.calendar -> changeView(CalendarFragment())
+            R.id.management -> changeView(ManagementFragment())
+            R.id.new_task -> changeView(NewTaskFragment())
+        }
+        return true
+    }
+
+    private fun changeView(fragment: Fragment) {
+        val manager = supportFragmentManager
+        val transition = manager.beginTransaction()
+        transition.replace(R.id.main_layout, fragment)
+        transition.commit()
     }
 }
